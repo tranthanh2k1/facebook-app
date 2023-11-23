1f1f85dd7b1fe1bc430ad1b2dc720bc3723c85ad
thanhtt <thanhtt@vmodev.com>
2023-11-23 14:42:16 +0700

    feat: setup tailwind and structure folter

---
+++ components/Hello.vue
@@ -0,0 +1,3 @@
+<template>
+	<div>Hello Vue qưeqeqwe</div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [],
+	mode: "jit",
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
